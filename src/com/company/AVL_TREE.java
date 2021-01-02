41759f75d17f6c4e3535ab8c03ef29d8f54501ff
me3eh <56845538+me3eh@users.noreply.github.com>
2021-01-02 14:20:07 +0100

    Add files via upload

---
+++ src/com/company/AVL_TREE.java
@@ -0,0 +1,253 @@
+package com.company;
+
+
+import java.util.Stack;
+
+public class AVL_TREE {
+    private Nodes root;
+    //setter korzenia
+    public void set_root(Nodes checking) {
+        root = checking;
+    }
+    //do wszystkich funkcji (rekurencja :( )
+    public Nodes get_Root() {
+        return root;
+    }
+    //aktualizacja wysokosci
+    private void updateHeight(Nodes being_checked) {
+//        int quick_math = 1 + max(get_Height_NULL_OR_INT(being_checked.getLeft()),
+        int LEFT = being_checked.getLeft() == null ? 0 : being_checked.getLeft().getHeight();
+        int RIGHT = being_checked.getRight() == null ? 0 : being_checked.getRight().getHeight();
+//        int quick_math = 1 + max( LEFT,RIGHT);
+        being_checked.setHeight(1 + Math.max(LEFT, RIGHT));
+    }
+
+    private int weight_for_rotation(Nodes being_checked) {
+        if (being_checked == null)
+            return 0;
+        int LEFT = being_checked.getLeft() == null ? 0 : being_checked.getLeft().getHeight();
+        int RIGHT = being_checked.getRight() == null ? 0 : being_checked.getRight().getHeight();
+        return LEFT - RIGHT;
+//        return get_Height_NULL_OR_INT(being_checked.getLeft()) - get_Height_NULL_OR_INT(being_checked.getRight());
+//        return leftINT - rightINT;
+    }
+
+    //    wszystkie rotacje :D
+    private Nodes RR(Nodes A) {
+//        System.out.println("RR");
+        Nodes B = A.getLeft();
+        Nodes II = B.getRight();
+        B.setRight(A);
+        A.setLeft(II);
+        updateHeight(A);
+        updateHeight(B);
+        return B;
+    }
+
+    private Nodes LL(Nodes A) {
+//        System.out.println("LL");
+        Nodes B = A.getRight();
+        Nodes II = B.getLeft();
+        B.setLeft(A);
+        A.setRight(II);
+        updateHeight(A);
+        updateHeight(B);
+        return B;
+    }
+
+    private Nodes RL(Nodes A) {
+//        System.out.println("RL");
+        Nodes B = A.getRight();
+        Nodes C = B.getLeft();
+        Nodes III = C.getRight();
+        Nodes II = C.getLeft();
+        C.setLeft(A);
+        C.setRight(B);
+        B.setLeft(III);
+        A.setRight(II);
+        updateHeight(A);
+        updateHeight(B);
+        updateHeight(C);
+        return C;
+    }
+
+    private Nodes LR(Nodes A) {
+//        System.out.println("LR");
+        Nodes B = A.getLeft();
+        Nodes C = B.getRight();
+        Nodes III = C.getRight();
+        Nodes II = C.getLeft();
+        C.setLeft(B);
+        C.setRight(A);
+        B.setRight(II);
+        A.setLeft(III);
+        updateHeight(A);
+        updateHeight(B);
+        updateHeight(C);
+        return C;
+    }
+
+    //dodanie elementu do drzewa. Zwraca łączenie, bo może być zastąpiony korzeń.
+    public Nodes insert(Nodes checking, String node_to_insert) {
+        if (checking == null) {
+            checking = new Nodes(node_to_insert);
+            return checking;
+        }
+        //skrót od greater - equal- lower
+        int gel = checking.getName_city().compareTo(node_to_insert);
+        if (gel > 0)
+            checking.setRight(insert(checking.getRight(), node_to_insert));
+        else if (gel < 0)
+            checking.setLeft(insert(checking.getLeft(), node_to_insert));
+        else
+            return checking;
+        updateHeight(checking);
+        int balance = weight_for_rotation(checking);
+        if (balance > 1) {
+            if (weight_for_rotation(checking.getLeft()) == -1)
+                return LR(checking);
+            else
+                return RR(checking);
+        } else if (balance < -1) {
+            if (weight_for_rotation(checking.getRight()) == 1)
+                return RL(checking);
+            else
+                return LL(checking);
+        }
+        return checking;
+    }
+
+    private Nodes find_Next_One(Nodes start) {
+        Nodes temp = start;
+        while (temp.getLeft() != null)
+            temp = temp.getLeft();
+        return temp;
+    }
+
+    public Nodes delete(Nodes checking, String node_to_delete) {
+        if (checking == null)
+            return checking;
+        int gel = checking.getName_city().compareTo(node_to_delete);
+        if (gel > 0)
+            checking.setRight(delete(checking.getRight(), node_to_delete));
+        else if (gel < 0)
+            checking.setLeft(delete(checking.getLeft(), node_to_delete));
+        else {
+            //jezeli brak dzieci, zastap dany wezel null'em
+            if (checking.getLeft() == null && checking.getRight() == null)
+                return null;
+            //jezeli tylko prawe dziecko, przylacz je do ojca
+            else if (checking.getLeft() == null)
+                return checking.getRight();
+            //jezeli tylko lewe dziecko, przylacz je do ojca
+            else if (checking.getRight() == null)
+                return checking.getLeft();
+            //jezeli dwoje dzieci, znajdz nastepnik, zamien jego nazwe z naszym rozpatrywanym wezlem i usun nastepnik
+            else {
+                //znajdowanie nastepnika
+                Nodes next_one = find_Next_One(checking.getRight());
+                //zastapienie nazwy miasta usuwanego wezla z nastepnikiem
+                String temp_name_city = next_one.getName_city();
+                checking.setName_city(temp_name_city);
+                //usuwanie nastepnika
+                checking.setRight(delete(checking.getRight(), temp_name_city));
+            }
+        }
+        //pozycjonowanie drzewa, aktualizacja wysokosci
+        updateHeight(checking);
+        int balance = weight_for_rotation(checking);
+        if (balance > 1) {
+            if (weight_for_rotation(checking.getLeft()) == -1)
+                return LR(checking);
+            else
+                return RR(checking);
+        }
+        else if (balance < -1) {
+            if (weight_for_rotation(checking.getRight()) == 1)
+                return RL(checking);
+            else
+                return LL(checking);
+        }
+        return checking;
+    }
+    /*
+    //pokazanie wszystkich elementów. Złożoność n, iteracyjnie ze stosem
+    public void show() {
+        if(root == null) {
+            System.out.println("NULL");
+            return;
+        }
+        Stack<Nodes> lil_stack = new Stack<>();
+        Stack<Integer> nr_of_space = new Stack<>();
+        lil_stack.push(root);
+        root.setLevel(0);
+        nr_of_space.push(0);
+        while (!lil_stack.isEmpty()) {
+            Nodes x = lil_stack.pop();
+            int y = nr_of_space.pop();
+            String word = "";
+            for (int i = 0; i < y; ++i)
+                word += "\t";
+            System.out.println((x == null) ? word + "NULL" : word + x.getName_city());
+            if (x == null)
+                continue;
+            if (x.getLeft() != null)
+                x.getLeft().setLevel(x.getLevel() + 1);
+            if (x.getRight() != null)
+                x.getRight().setLevel(x.getLevel() + 1);
+            for (int i = 0; i < 2; ++i)
+                nr_of_space.push(x.getLevel() + 1);
+            lil_stack.push(x.getRight());
+            lil_stack.push(x.getLeft());
+        }
+    }
+    */
+    //wypisanie drzewa. Rekurencyjnie
+    public void show2(Nodes origin, int number_space){
+        String word = "";
+        for (int i = 0; i < number_space; ++i)
+            word += "\t";
+        System.out.println((origin == null) ? word + "NULL" : word + origin.getName_city());
+        if(origin != null){
+            show2(origin.getLeft(), number_space+1);
+            show2(origin.getRight(), number_space+1);
+        }
+    }
+    //znalezienie danego elementu
+    public boolean find(Nodes origin, String being_finded) {
+        if (origin == null)
+            return false;
+        boolean found;
+        int gel = origin.getName_city().compareTo(being_finded);
+        if (gel > 0)
+            found = find(origin.getRight(), being_finded);
+        else if (gel < 0)
+            found = find(origin.getLeft(), being_finded);
+        else
+            found = true;
+        return found;
+    }
+
+    public int Count_prefix(Nodes checking, String Prefix_to_Find) {
+        if(checking == null)
+            return 0;
+        int count = 0;
+        int gel;
+        if(checking.getName_city().length() < Prefix_to_Find.length())
+            gel = checking.getName_city().compareTo(Prefix_to_Find);
+        else {
+            String substring_of_city = checking.getName_city().substring(0, Prefix_to_Find.length());
+            gel = substring_of_city.compareTo(Prefix_to_Find);
+        }
+        if(gel > 0)
+            count += Count_prefix(checking.getRight(), Prefix_to_Find);
+        else if(gel < 0)
+            count += Count_prefix(checking.getLeft(), Prefix_to_Find);
+        else{
+            count += 1;
+            count += Count_prefix(checking.getRight(), Prefix_to_Find);
+            count += Count_prefix(checking.getLeft(), Prefix_to_Find);
+        }
+            return count;
+    }
+}
